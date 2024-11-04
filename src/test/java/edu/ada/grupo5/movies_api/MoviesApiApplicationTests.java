a4f91b0a4302a80de194f713500269a39afc1140
Victot <victorfarian@gmail.com>
2024-11-04 09:38:41 -0300

    initializing repo

---
+++ src/test/java/edu/ada/grupo5/movies_api/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ada.grupo5.movies_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
