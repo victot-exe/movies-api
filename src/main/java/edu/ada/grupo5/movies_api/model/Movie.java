3cd63c1ad57043499eb98245de364907ad7ea64d
farchettiensis <farchettiensis@users.noreply.github.com>
2024-11-05 11:15:41 -0300

    feat: adiciona modelo base e entidade para filmes.
    - são versões iniciais, revisar depois.

--- src/main/java/edu/ada/grupo5/movies_api/model/Movie.java
+++ src/main/java/edu/ada/grupo5/movies_api/model/Movie.java
@@ -1,5 +1,29 @@
 package edu.ada.grupo5.movies_api.model;
 
-// TODO: implementar
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "movies")
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+// TODO: implementar/revisar
 public class Movie extends BaseModel {
+    @Column(nullable = false)
+    private String title;
+    private String director;
+    private String genre;
+    private Integer year;
+    private String country;
+    private String language;
+    private Integer duration;
+    @Column(unique = true)
+    private Integer tmdbId;
+    @Column(unique = true)
+    private String imdbId;
 }
