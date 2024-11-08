baaca1eeeda9e262b35fea3b8ef843bfd2893282
Nathan <nathan.llucasl@gmail.com>
2024-11-08 15:04:42 -0300

    SerieRepository e ajuste na classe Serie

--- src/main/java/edu/ada/grupo5/movies_api/service/SeriesService.java
+++ src/main/java/edu/ada/grupo5/movies_api/service/SeriesService.java
@@ -1,11 +1,13 @@
 package edu.ada.grupo5.movies_api.service;
 
+import edu.ada.grupo5.movies_api.Repositories.SerieRepository;
 import edu.ada.grupo5.movies_api.client.api.TMDBClientFeign;
 import edu.ada.grupo5.movies_api.dto.ResponseDTO;
-import edu.ada.grupo5.movies_api.dto.tmdb.SerieDTO;
 import edu.ada.grupo5.movies_api.dto.tmdb.ResultResponseDTO;
 import edu.ada.grupo5.movies_api.dto.tmdb.SerieDTO;
 import edu.ada.grupo5.movies_api.dto.tmdb.SerieNameIdDTO;
+import edu.ada.grupo5.movies_api.model.Serie;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +15,14 @@ import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//TODO: implementar restante que serao utilizados nas rotas
+//TODO: implementar restante que serao utilizados nas rotas e excecoes
 @Service
 public class SeriesService {
 
     @Autowired
     private final TMDBClientFeign tmdbClientFeign;
+    @Autowired
+    private SerieRepository serieRepository;
 
     @Autowired
     public SeriesService(TMDBClientFeign tmdbClientFeign) {
@@ -43,5 +47,22 @@ public class SeriesService {
         ResultResponseDTO<SerieDTO> serieDTOResultResponseDTO = tmdbClientFeign.getSerie(serieName, includeAdult, language, page);
         return serieDTOResultResponseDTO.getResults().stream().map(serie -> new SerieNameIdDTO(serie.getName(), serie.getId())).collect(Collectors.toList());
 
+    }
+
+    public ResponseDTO<SerieDTO> save(SerieDTO dto) {
+        Serie serie = convertFromDTO(dto);
+        serieRepository.save(serie);
+        return ResponseDTO.<SerieDTO>builder().message("Serie saved successfully")
+                .timestamp(Instant.now())
+                .data(dto)
+                .build();
+    }
+
+    @Transactional
+    public Serie convertFromDTO(SerieDTO dto) {
+        if (serieRepository.existsByTmdbId(dto.getId())) throw new RuntimeException();
+        return new Serie(dto.getName(), dto.getId(),
+                dto.isAdult(), dto.getOriginal_language(),
+                dto.getOriginal_name(), dto.getFirst_air_date(), dto.getVote_average());
     }
 }
