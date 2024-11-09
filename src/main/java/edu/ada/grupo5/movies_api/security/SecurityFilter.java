5fc679df999879ea1898823c8f08d0e9d8628584
victor <victor.fagundes586@gmail.com>
2024-11-08 21:42:12 -0300

    findUserByLogin -> findUserDetailsByLogin

--- src/main/java/edu/ada/grupo5/movies_api/security/SecurityFilter.java
+++ src/main/java/edu/ada/grupo5/movies_api/security/SecurityFilter.java
@@ -33,15 +33,11 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if (token != null) {
-            var login = tokenService.validateToken(token);
-            UserDetails userDetails = userService.findUserDetailsByLogin(login);
         try {
             var token = this.recoverToken(request);
             if (token != null) {
                 var login = tokenService.validateToken(token);
-                UserDetails userDetails = userService.findUserByLogin(login);
+                UserDetails userDetails = userService.findUserDetailsByLogin(login);
 
                 var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
