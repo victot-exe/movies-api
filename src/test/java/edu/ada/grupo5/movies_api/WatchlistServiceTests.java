7967e3abe73a495d8b06740c14c216e3866623e0
Lothar <lothar223@hotmail.com>
2024-11-28 09:33:16 -0300

    feat(WatchlistServiceTests): save watchlist test

--- src/test/java/edu/ada/grupo5/movies_api/WatchlistServiceTests.java
+++ src/test/java/edu/ada/grupo5/movies_api/WatchlistServiceTests.java
@@ -3,6 +3,8 @@ package edu.ada.grupo5.movies_api;
 import edu.ada.grupo5.movies_api.Repositories.WatchListRepository;
 import edu.ada.grupo5.movies_api.dto.WatchListDTO;
 import edu.ada.grupo5.movies_api.model.*;
+import edu.ada.grupo5.movies_api.service.MovieService;
+import edu.ada.grupo5.movies_api.service.SeriesService;
 import edu.ada.grupo5.movies_api.service.WatchListService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,9 +34,14 @@ public class WatchlistServiceTests {
     private WatchListService watchListService;
 
     @Mock
+    private MovieService movieService;
+
+    @Mock
+    private SeriesService seriesService;
+
+    @Mock
     private WatchListRepository watchListRepository;
 
-    private WatchList watchList;
 
 
     @BeforeEach
@@ -50,7 +58,49 @@ public class WatchlistServiceTests {
     }
 
     @Test
-    public void getAllWatchlist_deveBuscarWatchlistComSucesso(){
+    public void save_deveSalvarNaWatchlistComSucesso(){
+
+        String tmdbId = "123";
+        String title = "As tranças do rei careca";
+        MovieSerieEnum movieSerieEnum = MovieSerieEnum.MOVIE;
+        WatchListStatus watchListStatus = WatchListStatus.TO_WATCH;
+        boolean favorite = true;
+
+        WatchList watchListToSave = new WatchList(tmdbId, title, movieSerieEnum, watchListStatus, favorite);
+        watchListToSave.setUserId(1);
+
+        watchListService.save(tmdbId, title, movieSerieEnum, watchListStatus, favorite);
+
+        verify(watchListRepository, times(1)).save(watchListToSave);
+        verify(movieService, times(1)).saveMovieBySearch(Integer.parseInt(tmdbId));
+    }
+
+    @Test
+    public void save_deveChamarSaveMovieBySearchSeMovieSerieEnumIgualMovie(){
+
+        String tmdbId = "123";
+        MovieSerieEnum movieSerieEnum = MovieSerieEnum.MOVIE;
+
+        watchListService.save(tmdbId, "Super-Herói: O Filme", movieSerieEnum, WatchListStatus.WATCHED, true);
+
+        verify(movieService, times(1)).saveMovieBySearch(Integer.parseInt(tmdbId));
+        verifyNoInteractions(seriesService);
+    }
+
+    @Test
+    public void save_deveChamarSaveSerieBySearchSeMovieSerieEnumIgualSerie(){
+
+        String tmdbId = "123";
+        MovieSerieEnum movieSerieEnum = MovieSerieEnum.SERIE;
+
+        watchListService.save(tmdbId, "Super-Herói: O Filme", movieSerieEnum, WatchListStatus.WATCHED, true);
+
+        verify(seriesService, times(1)).saveSerieBySearch(Integer.parseInt(tmdbId));
+        verifyNoInteractions(movieService);
+    }
+
+    @Test
+    public void getAll_deveBuscarWatchlistComSucesso(){
         WatchList watchListItem = new WatchList("1", "A volta dos que não foram", MovieSerieEnum.MOVIE, WatchListStatus.WATCHED);
         when(watchListRepository.findAllByUserId(1)).thenReturn(Collections.singletonList(watchListItem));
 
@@ -58,7 +108,18 @@ public class WatchlistServiceTests {
 
         Assertions.assertNotNull(result);
         Assertions.assertEquals(1, result.size());
-        Assertions.assertEquals("A volta dos que não foram", result.get(0).getTitle());
+        Assertions.assertEquals("A volta dos que não foram", result.getFirst().getTitle());
+
+        verify(watchListRepository, times(1)).findAllByUserId(1);
+    }
+
+    @Test
+    public void getAll_deveRetonarListaVazia(){
+
+        List<WatchListDTO> result = watchListService.getAll();
+
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.isEmpty());
 
         verify(watchListRepository, times(1)).findAllByUserId(1);
     }
