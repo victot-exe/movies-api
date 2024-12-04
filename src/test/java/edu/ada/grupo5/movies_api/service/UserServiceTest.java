20eaa5192cff39c80bb2a8eb6e11e949f3257e93
Victot <victorfarian@gmail.com>
2024-12-04 12:35:46 -0300

    merging conflicts

--- src/test/java/edu/ada/grupo5/movies_api/service/UserServiceTest.java
+++ src/test/java/edu/ada/grupo5/movies_api/service/UserServiceTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
